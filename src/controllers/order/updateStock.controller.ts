0d3c831a6d048fcb8640f845bb94ba1f2b655d30
Leaeraso <leaeraso@gmail.com>
2024-10-21 12:12:47 -0300

    feat: eliminamos controlladores y servicios innecesarios

--- src/controllers/order/updateStock.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-const updateStock = async (_req: Request, _res: Response) => {
-
-}
-
-export default updateStock
--- src/services/order/index.services.ts
+++ src/services/order/index.services.ts
@@ -1,13 +1,11 @@
 import getOrdersByUserId from './getOrdersByUserId.services'
 import getOrderDetails from './getOrderDetails.services'
 import createOrder from './createOrder.services'
-import updateOrderStatus from './updateOrderStatus.services'
 import deleteOrder from './deleteOrder.services'
 
 export default {
   getOrdersByUserId,
   getOrderDetails,
   createOrder,
-  updateOrderStatus,
   deleteOrder
 }
