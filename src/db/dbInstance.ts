cd046072d16706f1c0e29d4acff8cf49753640ad
Leaeraso <leaeraso@gmail.com>
2024-12-05 11:19:33 -0300

    feat: puerto de base de datos

--- src/db/dbInstance.ts
+++ src/db/dbInstance.ts
@@ -1,20 +1,19 @@
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 
-// import createTables from './initTables'
-
 dotenv.config()
 
 const MYSQL_HOST: string = process.env.MYSQL_HOST!
 const MYSQL_USER: string = process.env.MYSQL_USER!
 const MYSQL_PASS: string = process.env.MYSQL_PASS!
 const MYSQL_DB: string = process.env.MYSQL_DB!
+const MYSQL_PORT: number = parseInt(process.env.MYSQL_PORT!)
 
 const sequelize = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASS, {
   host: MYSQL_HOST,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  port: MYSQL_PORT
 })
 
-// console.log('sequelize declaration:', sequelize)
 
 export { sequelize }
