0ac295194daaf0457f49f7c6ac0eda9463f44ff6
Leaeraso <leaeraso@gmail.com>
2024-12-06 10:56:49 -0300

    seedeando la bd

--- src/db/dbInstance.ts
+++ src/db/dbInstance.ts
@@ -3,12 +3,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const {MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB}= process.env
+const {MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_PORT}= process.env
 
 
 const sequelize = new Sequelize(MYSQL_DB!, MYSQL_USER!, MYSQL_PASS!, {
   host: MYSQL_HOST!,
   dialect: 'mysql',
+  port: Number(MYSQL_PORT),
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
--- src/seeders/20241202171217-create-order-product.js
+++ src/seeders/20241202171217-create-order-product.js
@@ -5,37 +5,37 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('OrderProduct', [
       {
-        orderId: 1,
+        orderId: 4,
         productId: 1,
         quantity: 2,
         subtotal: 5000,
       },
       {
-        orderId: 1,
+        orderId: 4,
         productId: 2,
         quantity: 1,
         subtotal: 2500,
       },
       {
-        orderId: 2,  
+        orderId: 5,  
         productId: 3, 
         quantity: 3,
         subtotal: 10500,
       },
       {
-        orderId: 2,
+        orderId: 5,
         productId: 4, 
         quantity: 2,
         subtotal: 7000,
       },
       {
-        orderId: 3,
+        orderId: 6,
         productId: 1,
         quantity: 1,
         subtotal: 2500,
       },
       {
-        orderId: 3,
+        orderId: 6,
         productId: 2,
         quantity: 1,
         subtotal: 2500,
