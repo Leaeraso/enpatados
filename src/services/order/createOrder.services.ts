46bf52e57d9a9af623d4cf2696257665e449d1d5
Leaeraso <leaeraso@gmail.com>
2024-12-14 17:23:10 -0300

    console error

--- src/services/order/createOrder.services.ts
+++ src/services/order/createOrder.services.ts
@@ -1,13 +1,13 @@
-import orderProductDTO from '../../dto/order/orderProductDTO'
-import errorHelper, { customError } from '../../helpers/error.helper'
-import orderModel from '../../models/order/order.models'
-import orderProductModel from '../../models/order/orderProduct.models'
-import productModel from '../../models/product/product.models'
-import dotenv from 'dotenv'
+import orderProductDTO from '../../dto/order/orderProductDTO';
+import errorHelper, { customError } from '../../helpers/error.helper';
+import orderModel from '../../models/order/order.models';
+import orderProductModel from '../../models/order/orderProduct.models';
+import productModel from '../../models/product/product.models';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const { PHONE_NUMBER } = process.env
+const { PHONE_NUMBER } = process.env;
 
 const createOrder = async (id: number, products: orderProductDTO[]) => {
   try {
@@ -15,31 +15,31 @@ const createOrder = async (id: number, products: orderProductDTO[]) => {
       throw errorHelper.badRequestError(
         'No hay productos en el carrito',
         'BAD_REQUEST_ERROR'
-      )
+      );
     }
-    const productIds = products.map((product) => product.id)
+    const productIds = products.map((product) => product.id);
 
     const productList = await productModel.findAll({
-      where: {id: productIds}
-    })
+      where: { id: productIds },
+    });
 
     const productMap = new Map(
       productList.map((product) => [product.id, product])
-    )
+    );
 
-    let total = 0
+    let total = 0;
 
     for (const item of products) {
-      const product = productMap.get(item.id)
+      const product = productMap.get(item.id);
 
       if (!product) {
         throw errorHelper.notFoundError(
           'Producto no encontrado',
           'NOT_FOUND_ERROR'
-        )
+        );
       }
 
-      total += product.price * item.quantity
+      total += product.price * item.quantity;
     }
 
     const newOrder = await orderModel.create({
@@ -47,41 +47,45 @@ const createOrder = async (id: number, products: orderProductDTO[]) => {
       total: total,
       status: 'pendiente',
       userId: id,
-      discount: 0
-    })
+      discount: 0,
+    });
 
     for (const item of products) {
-      const product = productMap.get(item.id)
+      const product = productMap.get(item.id);
 
       if (!product) {
         throw errorHelper.notFoundError(
           'Producto no encontrado',
           'NOT_FOUND_ERROR'
-        )
+        );
       }
 
       await orderProductModel.create({
         orderId: newOrder.orderNumber,
         productId: item.id,
         quantity: item.quantity,
-        subtotal: item.quantity * product.price
-      })
+        subtotal: item.quantity * product.price,
+      });
     }
 
-    const message = `Hola enpatados! Acabo de visitar su sitio web y he comprado unas medias, mi numero de orden es ${newOrder.orderNumber}`
-    const whatsAppUrl = `https://wa.me/${PHONE_NUMBER}?text=${encodeURIComponent(message)}`
+    const message = `Hola enpatados! Acabo de visitar su sitio web y he comprado unas medias, mi numero de orden es ${newOrder.orderNumber}`;
+    const whatsAppUrl = `https://wa.me/${PHONE_NUMBER}?text=${encodeURIComponent(
+      message
+    )}`;
 
-    return whatsAppUrl
+    return whatsAppUrl;
   } catch (error) {
     if (error instanceof customError) {
-      throw error
+      throw error;
     }
+
+    console.error(error);
 
     throw errorHelper.internalServerError(
       'Error al crear la orden de compra',
       'CREATE_ORDER_ERROR'
-    )
+    );
   }
-}
+};
 
-export default createOrder
+export default createOrder;
