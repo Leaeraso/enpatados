78310803c7aa74fd5e3fb0626523dc588a8f8050
Leaeraso <leaeraso@gmail.com>
2024-12-16 13:08:51 -0300

    mejorando queries de ordenes de compra

--- src/controllers/order/getOrdersByUserId.controller.ts
+++ src/controllers/order/getOrdersByUserId.controller.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express'
-import { customError } from '../../helpers/error.helper'
-import orderService from '../../services/order/index.services'
+import { Request, Response } from 'express';
+import { customError } from '../../helpers/error.helper';
+import orderService from '../../services/order/index.services';
 
 const getOrdersByUserId = async (req: Request, res: Response) => {
   try {
-    const userId = req.params.id
+    const userId = req.params.id;
 
-    const {page = 1, pageSize = 10} = req.query
+    const { page = 1, pageSize = 10 } = req.query;
 
-    const {orders, totalPages, count} = await orderService.getOrdersByUserId(Number(userId), Number(page), Number(pageSize))
+    const { orders, totalPages, count } = await orderService.getOrdersByUserId(
+      Number(userId),
+      Number(page),
+      Number(pageSize)
+    );
 
     res.status(200).json({
       data: orders,
@@ -16,16 +20,16 @@ const getOrdersByUserId = async (req: Request, res: Response) => {
         currentPage: +page,
         totalPages,
         totalRecords: count,
-        pageSize: +pageSize
-      }
-    })
+        pageSize: +pageSize,
+      },
+    });
   } catch (error) {
     if (error instanceof customError) {
-      res.status(error.httpStatus).json({ error: error.message })
+      res.status(error.httpStatus).json({ error: error.message });
     } else {
-      res.status(500).json({ message: 'internal server error' })
+      res.status(500).json({ message: 'internal server error' });
     }
   }
-}
+};
 
-export default getOrdersByUserId
+export default getOrdersByUserId;
--- src/services/order/getOrdersByUserId.services.ts
+++ src/services/order/getOrdersByUserId.services.ts
@@ -1,45 +1,65 @@
-import orderDTO from '../../dto/order/orderDTO'
-import errorHelper, { customError } from '../../helpers/error.helper'
-import orderModel from '../../models/order/order.models'
+import orderDTO from '../../dto/order/orderDTO';
+import errorHelper, { customError } from '../../helpers/error.helper';
+import orderModel from '../../models/order/order.models';
+import productModel from '../../models/product/product.models';
+import imageModel from '../../models/image/image.models';
 
-const getOrdersByUserId = async (id: number, page: number, pageSize: number) => {
+const getOrdersByUserId = async (
+  id: number,
+  page: number,
+  pageSize: number
+) => {
   try {
     const options = {
       limit: pageSize,
-      offset: (page - 1) * pageSize
-    }
+      offset: (page - 1) * pageSize,
+    };
 
-    const {count, rows} = await orderModel.findAndCountAll({
+    const { count, rows } = await orderModel.findAndCountAll({
       ...options,
       where: {
-        userId: id
-      }
-    })
+        userId: id,
+      },
+      include: [
+        {
+          model: productModel,
+          as: 'products',
+          attributes: ['name'],
+          include: [
+            {
+              model: imageModel,
+              as: 'images',
+              attributes: ['url'],
+            },
+          ],
+        },
+      ],
+    });
 
-    if(rows.length === 0) {
+    if (rows.length === 0) {
       throw errorHelper.notFoundError(
         'Ordenes no encontradas',
         'NOT_FOUND_ERROR'
-      )
+      );
     }
 
     const orders: orderDTO[] = rows.map((row) => {
-      return row.toJSON() as orderDTO
-    })
+      return row.toJSON() as orderDTO;
+    });
 
-    const totalPages = Math.ceil(count / pageSize)
+    const totalPages = Math.ceil(count / pageSize);
 
-    return {orders, totalPages, count}
+    return { orders, totalPages, count };
   } catch (error) {
     if (error instanceof customError) {
-      throw error
+      throw error;
     }
 
     throw errorHelper.internalServerError(
-      'Error al obtener la orden de compra',
+      'Error al obtener las ordenes de compra',
       'INTERNAL_SERVER_ERROR'
-    )
+    );
   }
-}
+};
 
-export default getOrdersByUserId
+export default getOrdersByUserId;
